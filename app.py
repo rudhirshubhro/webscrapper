d3ccd780a558f191bf4aef90653be3b32f9d1b5d
Akshay Sharma <akshay.sharma2@idsil.com>
2025-06-02 13:23:09 +0530

    made post api and extract bulk urls data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
 
@@ -17,20 +18,28 @@ def extract_ash_paper_info(url):
     authors = [tag.get_text(strip=True) for tag in soup.find_all('span', class_='author')]
     abstract_tag = soup.find('div', class_='abstract')
     abstract = abstract_tag.get_text(strip=True) if abstract_tag else ''
+    full_html = soup.prettify()
 
     return {
-        'title': title,
-        'session': session_info,
-        'authors': authors,
-        'abstract': abstract
+        # 'title': title,
+        # 'session': session_info,
+        # 'authors': authors,
+        'abstract': abstract,
+        'html': full_html
     }
 
-@app.route('/extract', methods=['GET'])
-def extract():
-    url = request.args.get('url')
-    if not url:
-        return jsonify({'error': 'No URL provided'}), 400
-    return jsonify(extract_ash_paper_info(url))
+@app.route('/extract', methods=['POST'])
+def extract_multiple():
+    data = request.get_json()
+    urls = data.get('urls') if data else None
+
+    if not urls or not isinstance(urls, list):
+        return jsonify({'error': 'Please provide a list of URLs in the "urls" field'}), 400
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        results = list(executor.map(extract_ash_paper_info, urls))
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
